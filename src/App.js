6acce95776bcc29a2feab2eb8bcc98fa0e42b794
aarticontractor <contaaru@gmail.com>
2023-05-30 21:01:02 -0700

    created parent component App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Element } from 'react-scroll';
+import { Container } from 'reactstrap';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar />
+        <Container>
+            <Element name="about"><About /></Element>
+            <Element name="portfolio"><Portfolio /></Element>
+            <Element name="contact"><Contact /></Element>
+            <Element name="resume"><Resume /></Element>
+        </Container>
+        <Footer />
     </div>
   );
 }
